ed03c46993373863352a4324aad5c28c6597201f
yongyang <2955520958@qq.com>
2026-04-02 12:57:16 +0800

    refactor: add while loop for robust user input validation

--- month_01/day02_library.py
+++ month_01/day02_library.py
@@ -1,6 +1,14 @@
 book_name = input("请输入借阅的书名：")
-original_price = float(input("请输入书本原价："))
-day_overdue = int(input("请输入逾期天数："))
+while True:
+    try:
+
+        original_price = float(input("请输入书本原价："))
+        day_overdue = int(input("请输入逾期天数："))
+        break
+
+    except ValueError:
+        print("警告：金额或天数必须是数字！别调皮，重新输入！\n")    
+
 fine = original_price * 0.01 * day_overdue
 
 print("--------------------------")
